28281638795cda989e974c4b5401b0a78c580275
Sanshiro Sakai <xosya3@gmail.com>
2015-09-20 08:10:47 +0900

    リクエスト受け取り用のAPI作成

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  skip_before_filter :require_valid_token, only: [:create, :update, :destroy]
+
   def index
     token_check
     @receivers = []
@@ -15,5 +17,41 @@ class RequestsController < ApplicationController
       sender = User.find(receiver_status.sender_id)
       @senders.push({name: sender.name, avatar: sender.avatar})
     end
+  end
+
+  def create
+    respond_to do |format|
+      @request = Request.new(request_params)
+
+      if @request.save
+        format.json { render nothing: true, status: :created }
+      else
+        logger.debug @request.errors.full_messages
+        format.json { render nothing: true, status: :bad_request }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @request.update(request_params)
+        format.json { render json: @request }
+      else
+        format.json { render json: @request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @request.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:sender_id, :receiver_id, :message, :status)
   end
 end
