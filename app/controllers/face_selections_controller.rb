8350d65db6ce9b7c96a6ec77596129e8d1468d04
Sanshiro Sakai <xosya3@gmail.com>
2015-09-20 02:58:32 +0900

    好みのタイプをすすめる性別を修正

--- app/controllers/face_selections_controller.rb
+++ app/controllers/face_selections_controller.rb
@@ -2,6 +2,6 @@ class FaceSelectionsController < ApplicationController
   skip_before_filter :require_valid_token
 
   def index
-    @face_selections = FaceSelection.where(gender: params[:id])
+    @face_selections = FaceSelection.where(gender: params[:id].to_i ^ 1)
   end
 end
